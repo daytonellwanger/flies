d2916720c42da645c7ff3ffcdbc60337699dbd42
Dayton Ellwanger <daytonellwanger@gmail.com>
2018-10-05 20:54:12 -0700

    Adding a dialog

---
+++ Fly.java
@@ -0,0 +1,13 @@
+import javax.swing.JDialog;
+
+public class Fly {
+	
+	private static final int DIALOG_SIZE = 400;
+
+	public static void main(String[] args) {
+		JDialog dialog = new JDialog();
+		dialog.setSize(DIALOG_SIZE, DIALOG_SIZE);
+		dialog.setVisible(true);
+	}
+
+}
