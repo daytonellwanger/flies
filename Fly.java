19ddc170fdb7782b953021a7fd09487ab0dc4feb
Dayton Ellwanger <daytonellwanger@gmail.com>
2018-10-05 21:39:29 -0700

    Back-and-forth animation

--- Fly.java
+++ Fly.java
@@ -3,29 +3,38 @@ import javax.swing.JDialog;
 public class Fly {
 	
 	private static final int DIALOG_SIZE = 400;
-	private static final int VELOCITY = 5;
+	private static final int VELOCITY = 50;
 	private static final int SLEEP_TIME = 100;
 	
 	private JDialog dialog;
+	
+	private int[] location;
+	private int direction = 1;
 
 	public static void main(String[] args) {
 		new Fly();
 	}
 	
 	public Fly() {
+		location = new int[2];
 		dialog = new JDialog();
 		dialog.setSize(DIALOG_SIZE, DIALOG_SIZE);
 		dialog.setVisible(true);
 		run();
 	}
 	
+	private void move() {
+		location[0] += direction * VELOCITY;
+		location[1] += direction * VELOCITY;
+		if (location[0] > 1000 || location[0] < 0) {
+			direction *= -1;
+		}
+	}
+	
 	private void run() {
-		int x = 0;
-		int y = 0;
 		while (true) {
-			dialog.setLocation(x, y);
-			x += VELOCITY;
-			y += VELOCITY;
+			dialog.setLocation(location[0], location[1]);
+			move();
 			try {
 				Thread.sleep(SLEEP_TIME);
 			} catch (Exception ex) {}
