88c32cffe183604700c5ccc94dad6e12f3d886d7
Dayton Ellwanger <daytonellwanger@gmail.com>
2018-10-10 19:44:21 -0700

    atan2 implementation of getAngle

--- Geometry.java
+++ Geometry.java
@@ -4,18 +4,7 @@ public class Geometry {
 	public static double getAngle(double[] from, double[] to) {
 		double dX = to[0] - from[0];
 		double dY = -(to[1] - from[1]);
-		if (dX == 0) {
-			return (dY > 0) ? Math.PI / 2 : 3 * Math.PI / 2;
-		}
-		double a = Math.atan(dY / dX);
-		if ((dX <= 0) && (dY >= 0)) { // Q2
-			a += Math.PI;
-		} else if ((dX <= 0) && (dY <=0)) { // Q3
-			a += Math.PI;
-		} else if ((dX >= 0) && (dY <= 0)) { // Q4
-			a += 2 * Math.PI;
-		}
-		return a;
+		return Math.atan2(dY, dX);
 	}
 	
 }
